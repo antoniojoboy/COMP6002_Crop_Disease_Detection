e20b17346c103e83e5acc599c30025c4044e6b79
antoniojoboy <joboyantonio@gmail.com>
2024-10-19 06:45:36 +0800

    uploaded best results

--- dataset/download.py
+++ dataset/download.py
@@ -1,10 +1,18 @@
 import os 
 
-os.system("pip install roboflow")
+# os.system("pip install roboflow")
 
 from roboflow import Roboflow
 rf = Roboflow(api_key="Wp8GYbxNZgLdlrtf1eCB")
+
+
+
 project = rf.workspace("zkamlasi-kamlasi-hj4wj").project("plantvillage-dataset")
 version = project.version(1)
+dataset = version.download("yolov11")
+                
+
+project = rf.workspace("galihnugraha").project("maize-disease-20240221")
+version = project.version(8)
 dataset = version.download("yolov11")
                 
