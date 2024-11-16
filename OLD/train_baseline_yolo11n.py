4ce6b059bb951acff9f4f2814eacab7871837f3b
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2024-11-16 18:08:18 +0800

    created testing data

---
+++ OLD/train_baseline_yolo11n.py
@@ -0,0 +1,25 @@
+"""
+This is to estabilish a baseline for the maize 
+"""
+
+from ultralytics import YOLO
+import os
+
+home = os.getcwd()
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+print(home)
+
+model_name = "yolo11n-seg" # segmentation
+dataset_name = "maize-uav-crop-disease"
+
+# Load a yolo11n model
+model = YOLO("yolo11n-seg.pt").to("cuda")
+
+# Start tuning hyperparameters for yolo11n training on the COCO8 dataset
+result_grid = model.train(
+    data=home+"/dataset/"+dataset_name+"/data.yaml"
+    ,device = "0,1"
+    ,epochs = 100
+    )
+
+model.save(model_name+"_trained_"+dataset_name+".pt")
